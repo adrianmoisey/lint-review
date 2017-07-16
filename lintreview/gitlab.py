072a7c88ad01b510c4b002d2b331c846cac8d1f8
Adrian Moisey <adrian@changeover.za.net>
2017-07-16 14:34:15 +0200

    Add gitlab module

---
+++ lintreview/gitlab.py
@@ -0,0 +1,27 @@
+import logging
+import gitlab
+from functools import partial
+
+log = logging.getLogger(__name__)
+
+
+
+def get_client(config):
+    """
+    Factory for the Github client
+    """
+    return gitlab.Gitlab('https://api.gitlab.com', config['GITHUB_OAUTH_TOKEN'])
+
+
+def get_repository(config, user, repo):
+    gl = get_client(config)
+    return gl.projects.get('{}/{}'.format(user, repo))
+
+
+def get_lintrc(repo, ref):
+    """
+    Download the .lintrc from a repo
+    """
+    log.info('Fetching lintrc file')
+    response = repo.repository_blob(ref, '.lintrc')
+    return response.decoded
