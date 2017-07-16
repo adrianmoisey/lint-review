72f7fd835992f6b834d698424cc711aa77420470
Adrian Moisey <adrian@changeover.za.net>
2017-07-16 12:09:18 +0200

    Web.py

--- lintreview/web.py
+++ lintreview/web.py
@@ -33,12 +33,54 @@ def start_review():
 
 
 def handle_gitlab_hook():
-    log.info("GitLab hook hit")
-    pass
+    try:
+        object_attributes = request.json["object_attributes"]
+        action = object_attributes["action"]
+        number = object_attributes["iid"]
+        base_repo_url = object_attributes["target"]["url"]
+        head_repo_url = object_attributes["source"]["url"]
+        head_repo_ref = object_attributes["source_branch"]
+        user = object_attributes["target"]["namespace"]
+        head_user = object_attributes["source"]["namespace"]
+        repo = object_attributes["target"]["name"]
+        head_repo = object_attributes["source"]["name"]
+
+    except Exception as e:
+        log.error("Got an invalid JSON body. '%s'", e)
+        return Response(status=403,
+                        response="You must provide a valid JSON body\n")
+
+    log.info("Received GitLub merge request notification for "
+             "%s %s, (%s) from: %s",
+             base_repo_url, number, action, head_repo_url)
+
+    if action not in ("open", "reopen", "close"):
+        log.info("Ignored '%s' action." % action)
+        return Response(status=204)
+
+    if action == "close":
+        return close_review(user, repo, pull_request) # FIXME
+
+    gl = gitlab.get_repository(app.config, head_user, head_repo)
+    try:
+        lintrc = gitlab.get_lintrc(gl, head_repo_ref)
+        log.debug("lintrc file contents '%s'", lintrc)
+    except Exception as e:
+        log.warn("Cannot download .lintrc file for '%s', "
+                 "skipping lint checks.", base_repo_url)
+        log.warn(e)
+        return Response(status=204)
+    try:
+        log.info("Scheduling pull request for %s/%s %s", user, repo, number)
+        process_pull_request.delay(user, repo, number, lintrc)  # FIXME
+    except:
+        log.error('Could not publish job to celery. Make sure its running.')
+        return Response(status=500)
+    return Response(status=204)
 
 
 def handle_github_hook():
-    event = request.headers.get('X-Github-Event')
+    event = request.headers.get('X-Gitlab-Event')
     if event == 'ping':
         return Response(status=200)
 
